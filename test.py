887c776b1c5d10fbd0f0680d0b72fbb3ba394563
imtiazBDSgit <imtiaz.khan.BDS@gmail.com>
2017-08-30 18:38:22 +0530

    converting test to test.exe
        Write your setup.py file.
    
        Open the command line (Start -> Run -> "cmd")
    
        Go to the location of your setup.py file and run python setup.py build

---
+++ test.py
@@ -0,0 +1,12 @@
+# Import the webbrowser module, it is used to
+# display various HTML documents to the user.
+import webbrowser
+
+ 
+# Second Input: It is the URL that you want
+# to open on the given time.
+url= "www.google.com"
+webbrowser.open(url)
+
+
+
